c808455d8ae7741633ec70a7e97810bcbba16870
lcf <lichengfeng.1996@qq.com>
2018-08-15 14:28:57 +0800

    ssm > git

--- src/com/lcf/controller/ProdutcController.java
+++ src/com/lcf/controller/ProdutcController.java
@@ -3,8 +3,6 @@ package com.lcf.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
